f8f3aa5df997c933eabd382d73473c591da94dc1
Andrew Sun <andrew@crossbowffs.com>
2023-01-09 19:38:22 -0800

    Add missing includes

--- panel-plugin/ddcplugin.c
+++ panel-plugin/ddcplugin.c
@@ -2,7 +2,9 @@
 #include <assert.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 #include <gtk/gtk.h>
